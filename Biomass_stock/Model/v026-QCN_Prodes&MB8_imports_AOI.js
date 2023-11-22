4a7afdd5d1bf75465b1725ecb028e6b5e74c6c91
Edriano Souza <79071899+souza-eab@users.noreply.github.com>
2023-11-22 14:48:21 -0300

    Create v026-QCN_Prodes&MB8_imports_AOI.js

---
+++ Biomass_stock/Model/v026-QCN_Prodes&MB8_imports_AOI.js
@@ -0,0 +1,50 @@
+var geometry = 
+    /* color: #d63000 */
+    /* shown: false */
+    ee.Geometry.MultiPoint(
+        [[-56.788474883614874, -6.8109693313360005],
+         [-56.840892146273625, -6.808312450730985]]),
+    mineracao = 
+    /* color: #d63000 */
+    /* shown: false */
+    ee.Geometry.Point([-56.81055492998406, -6.825727557434225]),
+    wetland_to_water = 
+    /* color: #98ff00 */
+    /* shown: false */
+    ee.Geometry.Point([-57.17734777865693, -18.476332512649037]),
+    geometry2 = 
+    /* color: #d63000 */
+    /* shown: false */
+    ee.Geometry.Point([-51.50586583719637, -8.542557141690569]),
+    final_errado = 
+    /* color: #98ff00 */
+    /* shown: false */
+    ee.Geometry.Point([-46.15384670100403, -12.519427429929886]),
+    incr_floresta_campestre = 
+    /* color: #d63000 */
+    /* shown: false */
+    ee.Geometry.Point([-44.96455500589469, -21.634449753345763]),
+    Savana = 
+    /* color: #d63000 */
+    /* shown: false */
+    ee.Geometry.Point([-40.82211429708013, -6.6372084744350826]),
+    F_Campestre = 
+    /* color: #98ff00 */
+    /* shown: false */
+    ee.Geometry.Point([-60.6827740604432, 3.866542148008096]),
+    savana_uso_savana = 
+    /* color: #d63000 */
+    /* shown: false */
+    ee.Geometry.Point([-40.92728571388169, -6.553909907312341]),
+    geometry3 = 
+    /* color: #d63000 */
+    /* shown: false */
+    ee.Geometry.Point([-45.13101546511709, -21.871281529333245]),
+    logging_2x = 
+    /* color: #d63000 */
+    /* shown: false */
+    ee.Geometry.Point([-53.554436277910064, -12.26016595869801]),
+    logging_fire = 
+    /* color: #98ff00 */
+    /* shown: false */
+    ee.Geometry.Point([-53.59941155867178, -12.268217661595257]);
